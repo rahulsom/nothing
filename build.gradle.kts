8f5191644dd202dde59578825fd49fb1c943f0c8
Rahul Somasunderam <rahul.som@gmail.com>
2025-09-07 18:38:56 -0700

    Revert "chore: Drop contacts altogether" (#190)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.rahulsom.waena.WaenaExtension
+import nebula.plugin.contacts.Contact
 
 plugins {
   id("com.github.rahulsom.waena.root") version "0.18.0"
@@ -6,6 +7,15 @@ plugins {
 
 allprojects {
   group = "io.github.rahulsom"
+}
+
+contacts {
+  validateEmails = true
+  addPerson("rahulsom@noreply.github.com", delegateClosureOf<Contact> {
+    moniker("Rahul Somasunderam")
+    roles("owner")
+    github("https://github.com/rahulsom")
+  })
 }
 
 waena {
