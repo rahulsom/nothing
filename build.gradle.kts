2171ed2758f54c540ccfce8bc573979c1fdb8562
Rahul Somasunderam <rahul.som@gmail.com>
2025-09-15 21:36:03 -0700

    Revert "chore: Use default author"

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,15 @@ allprojects {
   group = "io.github.rahulsom"
 }
 
+contacts {
+  validateEmails = true
+  addPerson("rahulsom@noreply.github.com", delegateClosureOf<Contact> {
+    moniker("Rahul Somasunderam")
+    roles("owner")
+    github("https://github.com/rahulsom")
+  })
+}
+
 waena {
   license.set(WaenaExtension.License.Apache2)
   publishMode.set(WaenaExtension.PublishMode.Central)
